5d2ee454620cb6ff08dbc964a938f43b1a8baad4
仲間太陽 <taiyoonakama@gmail.com>
2021-07-14 18:52:11 +0900

    5th-commit

--- database/migrations/2021_07_14_092410_create_todos_table.php
+++ database/migrations/2021_07_14_092410_create_todos_table.php
@@ -14,10 +14,10 @@ class CreateTodosTable extends Migration
     public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
-        $table->id();
-        $table->string('content');
-        $table->timestamp('created_at')->useCurrent->nullable();
-        $table->timestamp('updated_at')->useCurrent->nullable();
+            $table->id();
+            $table->string('content');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
         });
     }
 
