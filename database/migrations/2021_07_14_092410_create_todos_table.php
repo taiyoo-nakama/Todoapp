75761098ecc765b04d98a3b2b94231d57fd8b3ef
仲間太陽 <taiyoonakama@gmail.com>
2021-07-14 18:32:45 +0900

    4th-commit

--- database/migrations/2021_07_14_092410_create_todos_table.php
+++ database/migrations/2021_07_14_092410_create_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTodoTable extends Migration
+class CreateTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateTodoTable extends Migration
      */
     public function up()
     {
-        Schema::create('todo', function (Blueprint $table) {
-             $table->id();
-            $table->string('content');
-            $table->timestamp('created_at')->useCurrent()->nullable();
-            $table->timestamp('updated_at')->useCurrent()->nullable();
+        Schema::create('todos', function (Blueprint $table) {
+        $table->id();
+        $table->string('content');
+        $table->timestamp('created_at')->useCurrent->nullable();
+        $table->timestamp('updated_at')->useCurrent->nullable();
         });
     }
 
@@ -28,6 +28,6 @@ class CreateTodoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todo');
+        Schema::dropIfExists('todos');
     }
 }
--- database/seeders/TodosTableSeeder.php
+++ database/seeders/TodosTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Todo;
 
-class TodoTableSeeder extends Seeder
+class TodosTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
