f58a03b616bf5350b839d1e7edce69a046e262c6
仲間太陽 <taiyoonakama@gmail.com>
2021-07-15 20:20:22 +0900

    17th-commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -14,19 +14,14 @@ class TodoController extends Controller
     }
     public function create(Request $request){
         $this->validate($request, Todo::$rules);
-        $todo = new Todo;
         $form = $request->all();
-        unset($form['_token_']);
-        $todo->fill($form)->save();
+        Todo::create($item);
         return redirect('/');
     }
-    public function update(Request $request)
-    {
-        $this->validate($request,Todo::$rules);
-        $todo = Todo::find($request->id);
+    public function update(Request $request){
+        $this->validate($request, Todo::$rules);
         $form = $request->all();
-        unset($form['_token_']);
-        $todo->fill($form)->save();
+        Todo::where('id',$request->id)->update($item);
         return redirect('/');
     }
     public function delete(Request $request)
